e97ff185b7905542d39a254e430a9f8fb8077783
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-07-04 19:39:41 +0100

    :recycle: Move Domain Class

--- BankKata/AccountService.cs
+++ BankKata/AccountService.cs
@@ -43,32 +43,3 @@ public class AccountService
         }
     }
 }
-
-public class AccountTransaction
-{
-    int amount;
-    string date;
-    int balance;
-
-    public AccountTransaction(int amount, string date, int balance)
-    {
-        this.amount = amount;
-        this.date = date;
-        this.balance = balance;
-    }
-
-    public int getAmount()
-    {
-        return this.amount;
-    }
-    
-    public string getDate()
-    {
-        return this.date;
-    }
-
-    public int getBalance()
-    {
-        return this.balance;
-    }
-}
---
+++ BankKata/AccountTransaction.cs
@@ -0,0 +1,30 @@
+﻿namespace BankKata;
+
+public class AccountTransaction
+{
+    int amount;
+    string date;
+    int balance;
+
+    public AccountTransaction(int amount, string date, int balance)
+    {
+        this.amount = amount;
+        this.date = date;
+        this.balance = balance;
+    }
+
+    public int getAmount()
+    {
+        return this.amount;
+    }
+    
+    public string getDate()
+    {
+        return this.date;
+    }
+
+    public int getBalance()
+    {
+        return this.balance;
+    }
+}
