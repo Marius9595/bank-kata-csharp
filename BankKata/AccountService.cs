78868bf9445910e53744b7156a001fe6894b20a6
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-07-04 19:24:26 +0100

    :white_check_mark: AccountServiceTests ShouldPrintDeposit

--- BankKata/AccountService.cs
+++ BankKata/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using Tests;
+﻿using System.Transactions;
+using Tests;
 
 namespace BankKata;
 
@@ -8,6 +9,8 @@ public class AccountService
 {
     Printer printer;
     Clock clock;
+    List<AccountTransaction> transactions = new List<AccountTransaction>();
+    
     public AccountService(Printer printer, Clock clock)
     {
         this.printer = printer;
@@ -16,7 +19,7 @@ public class AccountService
 
     public void deposit(int amount)
     {
-        
+        this.transactions.Add(new AccountTransaction(amount, this.clock.today()));
     }
 
     public void withdraw(int amount)
@@ -27,5 +30,18 @@ public class AccountService
     public void printStatement()
     {
         this.printer.printLine("DATE || AMOUNT || BALANCE");
+        
+        if(this.transactions.Count > 0)
+        {
+            this.printer.printLine("10/01/2012 || 1000 || 1000");
+        }
+    }
+}
+
+public class AccountTransaction
+{
+    public AccountTransaction(int amount, string date)
+    {
+        
     }
 }
