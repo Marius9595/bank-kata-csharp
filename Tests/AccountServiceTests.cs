619cc6ffd9da8288e0ab7c39ac17b022d8447c4b
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-07-04 19:54:09 +0100

    :white_check_mark: AccountServiceTests ShouldPrintWithDraw AcceptanceTest

--- Tests/AccountServiceTests.cs
+++ Tests/AccountServiceTests.cs
@@ -11,7 +11,7 @@ public class AccountServiceTests
     {
         var printer = Substitute.For<Printer>();
         var clock = Substitute.For<Clock>();
-        clock.today().Returns("01/04/2014", "02/04/2014", "10/04/2014");
+        clock.today().Returns("10/01/2012", "13/01/2012", "14/01/2012");
         var accountService = new AccountService(printer, clock);
         accountService.deposit(1000);
         accountService.deposit(2000);
@@ -25,7 +25,7 @@ public class AccountServiceTests
             printer.Received().printLine("14/01/2012 || -500 || 2500");
             printer.Received().printLine("13/01/2012 || 2000 || 3000");
             printer.Received().printLine("10/01/2012 || 1000 || 1000");
-        });
+       });
     }
     
     [Fact]
